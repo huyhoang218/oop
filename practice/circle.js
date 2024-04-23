74d9b22b511bc12b96b9e1a34eecfc36f94f20cd
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-04-23 13:18:14 +0700

    update

--- practice/circle.js
+++ practice/circle.js
@@ -1,13 +1,34 @@
-let Circle = function(radius){
-    this.radius = radius
-    this.getRadius = function(){
-        return radius
-    } 
-    this.getArea = function(){
+// let Circle = function(radius){
+//     this.radius = radius
+//     this.getRadius = function(){
+//         return radius
+//     } 
+//     this.getArea = function(){
+//         return Math.PI * radius * radius
+//     }
+// }
+// let circle = new Circle(2)
+// let radius = circle.getRadius()
+// let area = circle.getArea()
+// alert(`radius is ${radius}, area of circle ${area}`)
+
+class Circle {
+    constructor(radius, color){
+        this.radius = radius
+        this.color = color
+    }
+    getRadius(){
+        return this.radius
+    }
+    getArea(){
         return Math.PI * radius * radius
     }
+    getColor(){
+        return this.color
+    }
 }
-let circle = new Circle(2)
+let circle = new Circle(10, 'red')
 let radius = circle.getRadius()
 let area = circle.getArea()
-alert(`radius is ${radius}, area of circle ${area}`)
+let color = circle.getColor()
+alert(`radius is ${radius}, area of circle ${area}, color is ${color}`)
