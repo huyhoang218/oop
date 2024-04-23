ab803bf4536c2a11fca6dd975dda2ddef177ca5f
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-04-23 08:52:25 +0700

    update

--- practice/createClassDate.js
+++ practice/createClassDate.js
@@ -11,9 +11,32 @@ let MyDate = function (day, month, year) {
     this.getYear = function () {
         return this.year
     }
+    this.setDay = function (day) {
+        this.day = day
+    }
+    this.setMonth = function (month) {
+        this.month = month
+    }
+    this.setYear = function (year) {
+        this.year = year
+    }
+    this.setDate = function(day, month, year){
+        this.day = day
+        this.month = month
+        this.year = year
+    }
+    this.toString = function(){
+        str = day + '/' + month + '/' + year
+        return this.str
+    }
 }
- let date = new MyDate (22, 4, 2024)
- let day = date.getDay()
- let month = date.getMonth()
- let year = date.getYear()
- alert(day + '/' + month + '/' + year)
+let date = new MyDate(22, 4, 2024)
+// date.setDay(23)
+// date.setMonth(3)
+// date.setYear(2021)
+let day = date.getDay()
+let month = date.getMonth()
+let year = date.getYear()
+// alert(day + '/' + month + '/' + year)
+date.setDate()
+alert(date)
